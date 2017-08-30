02874c0be3e8ae49131eef653eda808eb142d312
kfang <kfang1233@gmail.com>
2017-08-30 18:20:27 -0400

    Added working React application

--- cws-schedule-gcal/src/App.js
+++ cws-schedule-gcal/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import { CLIENT_ID, DISCOVERY_DOCS, SCOPES, getUpcomingEvents, addClass } from './Api.js'
 import getGApi from 'google-client-api'
 import periods from './periods.js'
-
+import { FormComponent } from './FormComponent.js'
 
 // material design stuff
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
@@ -12,120 +12,117 @@ import injectTapEventPlugin from 'react-tap-event-plugin';
 import getMuiTheme from 'material-ui/styles/getMuiTheme'
 
 injectTapEventPlugin();
-
 const muiTheme = getMuiTheme({
-  margin: 0,
-  appBar: {
-    height: 70
-  }
+	margin: 0,
+	appBar: {
+		height: 70
+	}
 })
 
 var sampleClass = {
-  day: periods.tuesday,
-  period: periods.tuesday.first,
-  name: "Relativity",
-  room: '4B',
-  date: '2017-09-12',
-  recurrence: "weekly" // 'single' or 'weekly'
+	day: periods.tuesday,
+	period: periods.tuesday.first,
+	name: "Relativity",
+	room: '4B',
+	date: '2017-09-12',
+	recurrence: "weekly" // 'single' or 'weekly'
 }
 
 
 class App extends Component{
-  constructor(props) {
-    super(props);
-    this.state = {
-      showAuthButton: false,
-      showSignOutButton: false
-    };
-    this.initClient = this.initClient.bind(this);
-    this.updateSigninStatus = this.updateSigninStatus.bind(this);
-    this.createAndAddClass = this.createAndAddClass.bind(this);
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			showAuthButton: false,
+			showSignOutButton: false
+		};
+		this.initClient = this.initClient.bind(this);
+		this.updateSigninStatus = this.updateSigninStatus.bind(this);
+		this.createAndAddClass = this.createAndAddClass.bind(this);
+	}
 
-  handleAuthClick(){
-    getGApi().then((gapi) => {
-      gapi.auth2.getAuthInstance().signIn();
-    })
-  }
-  
-  handleSignoutClick(){
-    getGApi().then((gapi) => {
-      gapi.auth2.getAuthInstance().signOut();
-    })
-  }
+	handleAuthClick(){
+		getGApi().then((gapi) => {
+			gapi.auth2.getAuthInstance().signIn();
+		})
+	}
+	
+	handleSignoutClick(){
+		getGApi().then((gapi) => {
+			gapi.auth2.getAuthInstance().signOut();
+		})
+	}
 
-  handleClientLoad() {
-    getGApi().then((gapi) => {
-      gapi.load('client:auth2', this.initClient);
-    })
-  }
+	handleClientLoad() {
+		getGApi().then((gapi) => {
+			gapi.load('client:auth2', this.initClient);
+		})
+	}
 
-  updateSigninStatus(isSignedIn) {
-    if (isSignedIn) {
-      this.setState({
-        showAuthButton: false,
-        showSignOutButton: true
-      })
+	updateSigninStatus(isSignedIn) {
+		if (isSignedIn) {
+			this.setState({
+				showAuthButton: false,
+				showSignOutButton: true
+			})
 
-      getUpcomingEvents()
-    } else {
-      this.setState({
-        showAuthButton: true,
-        showSignOutButton: false
-      })
-    }
-  }
+			getUpcomingEvents()
+		} else {
+			this.setState({
+				showAuthButton: true,
+				showSignOutButton: false
+			})
+		}
+	}
 
-  initClient() {
-    var _this = this;
-    getGApi().then((gapi) => {
-      gapi.client.init({
-        discoveryDocs: DISCOVERY_DOCS,
-        clientId: CLIENT_ID,
-        scope: SCOPES
-      }).then(function () {
-        console.log(gapi);
-        gapi.auth2.getAuthInstance().isSignedIn.listen(_this.updateSigninStatus);
-        _this.updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
-      });
-    })
-  }
+	initClient() {
+		var _this = this;
+		getGApi().then((gapi) => {
+			gapi.client.init({
+				discoveryDocs: DISCOVERY_DOCS,
+				clientId: CLIENT_ID,
+				scope: SCOPES
+			}).then(function () {
+				console.log(gapi);
+				gapi.auth2.getAuthInstance().isSignedIn.listen(_this.updateSigninStatus);
+				_this.updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
+			});
+		})
+	}
 
-  componentDidMount(){
-    this.handleClientLoad();
-  }
+	componentDidMount(){
+		this.handleClientLoad();
+	}
 
-  createAndAddClass() {
-    addClass(sampleClass)
-  }
+	createAndAddClass(classInfo) {
+		addClass(classInfo)
+	}
 
-  render() {
-    let authButton = <RaisedButton label="Authorize" onClick={this.handleAuthClick.bind(this)} primary={true} />
-    let signOutButton = <RaisedButton label="Sign out" onClick={this.handleSignoutClick.bind(this)} secondary={true} />
-    return(
-      <MuiThemeProvider muiTheme={muiTheme}>
-      <div>
-        <AppBar
-          showMenuIconButton={false}
-          title="Commonwealth School Google Calendar"/>
-          <div style={{margin: 8}}>
-            {this.state.showAuthButton ? authButton : null}
-            {this.state.showSignOutButton ? (
-              <div>
-                <RaisedButton 
-                  label="Add Sample Class"
-                  primary={true}
-                  onClick={this.createAndAddClass}
-                  style={{marginBottom: 8}}
-                /><br/>
-                {signOutButton}
-              </div>
-             ) : null}
-          </div>
-      </div>
-    </MuiThemeProvider>
-    )
-  }
+	render() {
+		let authButton = <RaisedButton label="Authorize" onClick={this.handleAuthClick.bind(this)} primary={true} style={{marginLeft: 24}} />
+		let signOutButton = <RaisedButton label="Sign out" onClick={this.handleSignoutClick.bind(this)} secondary={true} style={{marginLeft: 24}} />
+		return(
+			<MuiThemeProvider muiTheme={muiTheme}>
+			<div>
+				<AppBar
+					showMenuIconButton={false}
+					title="Commonwealth GCal Scheduler"/>
+					<div style={{margin: 8}}>
+						{this.state.showAuthButton ? authButton : null}
+						{this.state.showSignOutButton ? (
+							<div>
+								<FormComponent
+									handleSubmit={ (classInfo) => { addClass(classInfo) } }
+									style={{marginBottom: 0}}/>
+								<br/>
+								{signOutButton}
+							</div>
+						 ) : null}
+					</div>
+			</div>
+		</MuiThemeProvider>
+		)
+	}
 }
 
 export default App
